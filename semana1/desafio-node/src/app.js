d33b61a3ca3dd1ef041744886e9e103f32e5a02c
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2020-04-16 00:24:50 -0300

    Create delete end-point update tests

--- semana1/desafio-node/src/app.js
+++ semana1/desafio-node/src/app.js
@@ -84,8 +84,18 @@ app.put(
   }
 );
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateRepositoryId, (request, response) => {
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found." });
+  } else {
+    repositories.splice(repositoryIndex, 1);
+    return response.status(204).send();
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
